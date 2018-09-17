1143532622f01eadc8a2898a4e8277f144cf1c37
gaoyanru <gaoyr@i-counting.cn>
2018-09-17 16:20:49 +0800

    [feat] 我的客资 related to issue #2

--- src/modules/customer/detail.tsx
+++ src/modules/customer/detail.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
-import { Row, Col, Input } from 'antd'
+import { Row, Col, Input, Select } from 'antd'
+const Option = Select.Option
 class Detail extends React.Component {
   public render () {
+    const selectBefore = (
+      <Select defaultValue="1" style={{ width: 90 }}>
+        <Option value="1">公司名</Option>
+        <Option value="2">法人</Option>
+        <Option value="2">特殊公司</Option>
+      </Select>
+    )
     return (
       <div>
         <Row>
