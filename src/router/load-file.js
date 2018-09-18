aa57427e5151c161d7393dca3fb700ffc2b768dc
gaoyanru <gaoyr@i-counting.cn>
2018-09-18 10:33:02 +0800

    [feat] 我的客资 related to issue #2

--- src/router/load-file.js
+++ src/router/load-file.js
@@ -1,3 +1,4 @@
 export { default as loadIndex } from 'bundle-loader?lazy&name=[name]!@/modules'
 export { default as loadCustomer } from 'bundle-loader?lazy&name=[name]!@/modules/customer'
+export { default as loadBusiness } from 'bundle-loader?lazy&name=[name]!@/modules/business'
 export { default as loadSigned } from 'bundle-loader?lazy&name=[name]!@/modules/signed'
