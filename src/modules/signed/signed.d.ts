f12b405a6ab1b1ef6555d217217b797a34afaa75
gaoyanru <gaoyr@i-counting.cn>
2018-09-19 20:55:29 +0800

    [feat] 签约客户公海页面 related to issue #2

---
+++ src/modules/signed/signed.d.ts
@@ -0,0 +1,10 @@
+export interface DetailProps {
+  customerName: string
+  contactPerson: string
+  area: string
+  flowPerson: string
+  operatingAccouting: string
+  createTime: string
+  startTime: string
+  endTime: string
+}
