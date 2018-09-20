ac300ee530ea4220a809ca374af3347a79e8b43e
gaoyanru <gaoyr@i-counting.cn>
2018-09-20 20:10:19 +0800

    [feat] 签约客户 related to issue #2

--- src/modules/signed/OperateList.tsx
+++ src/modules/signed/OperateList.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
-import { Table } from 'antd'
+import { Table, Radio } from 'antd'
 import { ColumnProps } from 'antd/lib/table'
+const RadioGroup = Radio.Group
 export interface DetailProps {
   opetarePerson: string
   opetareTime: string
   opetareContent: string
 }
 interface States {
-  data: DetailProps[]
+  data: DetailProps[],
+  current: number
 }
 export default class extends React.Component {
   public state: States = {
-    data: []
+    data: [],
+    current: 1
+  }
+  public onChange (e: any) {
+    this.setState({
+      current: e.target.value,
+    })
   }
   public columns: ColumnProps<DetailProps>[] = [{
     title: '操作人',
@@ -25,12 +33,19 @@ export default class extends React.Component {
   }]
   public render () {
     return (
-      <Table
-        columns={this.columns}
-        dataSource={this.state.data}
-        bordered
-        rowKey={'opetarePerson'}
-      />
+      <div>
+        <RadioGroup onChange={this.onChange} value={this.state.current}>
+          <Radio value={1}>操作记录</Radio>
+          <Radio value={2}>线索记录</Radio>
+        </RadioGroup>
+        <Table
+          columns={this.columns}
+          dataSource={this.state.data}
+          bordered
+          rowKey={'opetarePerson'}
+        />
+      </div>
+      
     )
   }
 }
