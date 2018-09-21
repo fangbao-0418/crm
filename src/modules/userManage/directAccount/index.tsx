aa10027aab2c6f6f55358db9a39f69f3066e878a
DESKTOP-DEV5V3U\wangye <hongri@i-counting.cn>
2018-09-21 15:44:28 +0800

    [feat]部分部门代码提交----代码提交 related to issue #4

--- src/modules/userManage/directAccount/index.tsx
+++ src/modules/userManage/directAccount/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import ContentBox from '@/modules/common/content'
 
 class Main extends React.Component {
 
   public render () {
     return (
       <div>
-        直营账号
+        <ContentBox title='直营账号'></ContentBox>
       </div>
     )
   }
