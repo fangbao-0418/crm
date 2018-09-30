58455a86ddd6678c74c59bbbf7a6b7e229ae1ba0
zhangmenglong <zhangml@i-counting.cn>
2018-09-30 10:25:44 +0800

    [feat] 部门逻辑调整 related to issue #3

--- src/modules/center-user-manage/department/index.tsx
+++ src/modules/center-user-manage/department/index.tsx
@@ -7,19 +7,21 @@ const styles = require('./style')
 const FormItem = Form.Item
 
 interface State {
-  val: string
-  help: string
-  title: string
-  visible: boolean
+  val: string // 弹窗部门值
+  mode: 'add' | 'addRoot' | 'modify' // 添加子部门 | 添加根部门 | 修改部门
+  visible: boolean // 是否展示弹窗
+  verification: 'empty' | 'same' | 'normal' // 部门为空 | 部门重复 | 正常状态
   dataSource: any[]
+  currentInfo: any // 当前选中部门信息
 }
 
-class Main extends React.Component<any, State> {
-  public state = {
-    val: '', // 弹窗权限值
-    help: '', // 验证提示
-    title: '', // 弹窗标题
-    visible: false, // 弹窗显示
+class Main extends React.Component {
+  public state: State = {
+    val: '',
+    mode: 'add',
+    visible: false,
+    verification: 'normal',
+    currentInfo: {},
     dataSource: [
       {
         id: 1,
@@ -42,8 +44,10 @@ class Main extends React.Component<any, State> {
   }
 
   // 修改、添加部门
-  public setDepartment = (title: string, name?: string) => {
-    this.setState({visible: true, title, val: name || ''})
+  public setDepartment = (mode: 'add' | 'addRoot' | 'modify', currentInfo?: any) => {
+    const val = mode === 'modify' ? currentInfo.name : ''
+    currentInfo = currentInfo || {}
+    this.setState({visible: true, mode, currentInfo, val})
   }
 
   // 禁用部门
@@ -61,7 +65,34 @@ class Main extends React.Component<any, State> {
     })
   }
 
+  // 设置错误信息
+  public getErrorInfo: any = (verification: 'empty' | 'same' | 'normal') => {
+    let errorInfo
+    if (verification === 'empty') {
+      errorInfo = {help: '部门名称不能为空', validateStatus: 'error'}
+    } else if (verification === 'same') {
+      errorInfo = {help: '部门名称重复', validateStatus: 'error'}
+    } else if (verification === 'normal') {
+      errorInfo = {help: ''}
+    }
+    return errorInfo
+  }
+
+  // 获取弹窗标题
+  public getTitle: any = (mode: any) => {
+    let title
+    if (mode === 'add') {
+      title = '添加子部门'
+    } else if (mode === 'addRoot') {
+      title = '添加根部门'
+    } else if (mode === 'modify') {
+      title = '修改部门'
+    }
+    return title
+  }
+
   public render () {
+    const {verification, mode, currentInfo} = this.state
     const columns = [{
       title: '部门名称',
       dataIndex: 'name'
@@ -70,9 +101,9 @@ class Main extends React.Component<any, State> {
       render: (val: any, info: any) => {
         return (
           <div>
-            <a onClick={() => {this.setDepartment('添加部门')}}>添加子部门</a>
+            <a onClick={() => {this.setDepartment('add', info)}}>添加子部门</a>
             <Divider type='vertical'/>
-            <a onClick={() => {this.setDepartment('修改部门', info.name)}}>修改</a>
+            <a onClick={() => {this.setDepartment('modify', info)}}>修改</a>
             <Divider type='vertical'/>
             <a onClick={() => {this.forbidDepartment()}}>禁用</a>
             <Divider type='vertical'/>
@@ -88,7 +119,7 @@ class Main extends React.Component<any, State> {
         rightCotent={(
           <AddButton
             title='添加一级部门'
-            onClick={() => {this.setDepartment('添加部门')}}
+            onClick={() => {this.setDepartment('addRoot')}}
           />
         )}
       >
@@ -99,38 +130,41 @@ class Main extends React.Component<any, State> {
           rowKey='id'
           pagination={false}
         />
-        <Modal
-          title={this.state.title}
-          visible={this.state.visible}
-          destroyOnClose={true}
-          onOk={() => {
-            if (this.state.val === '') {
-              this.setState({help: '部门名称不能为空'})
-              return
-            }
-            this.setState({visible: false})
-          }}
-          onCancel={() => {
-            this.setState({visible: false, help: '', val: ''})
-          }}
-        >
-          <FormItem
-           required
-           label='部门名称'
-           labelCol={{span: 8}}
-           wrapperCol={{span: 10}}
-           help={<span style={{color: 'red'}}>{this.state.help}</span>}
+        {
+          this.state.visible &&
+          <Modal
+            title={this.getTitle(this.state.mode)}
+            visible={true}
+            destroyOnClose={true}
+            onOk={() => {
+              if (this.state.val === '') {
+                this.setState({verification: 'empty'})
+                return
+              }
+              this.setState({visible: false})
+            }}
+            onCancel={() => {
+              this.setState({visible: false, verification: 'normal'})
+            }}
           >
-           <Input
-             defaultValue={this.state.val}
-             onChange={(e) => {
-               this.setState({
-                 val: e.target.value
-               })
-             }}
-           />
-          </FormItem>
-        </Modal>
+            <FormItem
+              required
+              label='部门名称'
+              labelCol={{span: 8}}
+              wrapperCol={{span: 10}}
+              {...this.getErrorInfo(verification)}
+            >
+              <Input
+                defaultValue={this.state.val}
+                onChange={(e) => {
+                  this.setState({
+                    val: e.target.value
+                  })
+                }}
+              />
+            </FormItem>
+          </Modal>
+        }
       </ContentBox>
     )
   }
