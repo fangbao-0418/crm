d8857e2a1349d36fd1b70693161a27dca1709c86
fangbao <fangbao@i-counting.cn>
2018-10-08 16:04:24 +0800

    :

--- src/modules/customer-set/main/Region.tsx
+++ src/modules/customer-set/main/Region.tsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import { Select } from 'antd'
-import DropDown from 'pilipa/libs/dropdown'
+import { LabeledValue } from 'antd/lib/select'
 import { fetchRegion } from '@/modules/common/api'
 const styles = require('./style')
 const Option = Select.Option
+interface Props {
+  onChange?: (values: LabeledValue[]) => void
+}
 interface State {
   cityName: string
   cityList: Common.RegionProps[]
   areaName: string
   areaList: Common.RegionProps[]
+  areaValues: LabeledValue[]
 }
-class Main extends React.Component {
+class Main extends React.Component<Props> {
   public state: State = {
     cityName: '',
     areaName: '',
     cityList: [],
-    areaList: []
+    areaList: [],
+    areaValues: []
   }
+  public areaList: Common.RegionProps[] = []
   public componentWillMount () {
     fetchRegion().then((res) => {
       this.setState({
@@ -29,38 +35,66 @@ class Main extends React.Component {
   }
   public handleCityChange (values: {key: string, label: string}[]) {
     const labels: string[] = []
+    const p: Promise<any>[] = []
+    values.forEach((item) => {
+      labels.push(item.label)
+      p.push(
+        fetchRegion({
+          parentId: item.key,
+          level: 2
+        })
+      )
+    })
+    Promise.all(p).then((res) => {
+      let arr: Common.RegionProps[] = []
+      const areaValues: LabeledValue[] = []
+      res.forEach((item) => {
+        item.forEach((item2: Common.RegionProps) => {
+          areaValues.push({
+            key: item2.code,
+            label: item2.name
+          })
+        })
+        arr = arr.concat(item)
+      })
+      if (this.props.onChange) {
+        this.props.onChange(areaValues)
+      }
+      this.setState({
+        areaValues,
+        areaList: arr
+      })
+    })
+    this.setState({
+      cityName: labels.join(','),
+      areaName: '全部'
+    })
+  }
+  public handleAreaChange (values: {key: string, label: string}[]) {
+    const labels: string[] = []
     values.forEach((item) => {
       labels.push(item.label)
     })
     this.setState({
-      cityName: labels.join(',')
+      areaValues: values,
+      areaName: labels.join(',')
     })
-    // this.handleChange({
-    //   key: 'cityCode',
-    //   value: value.key
-    // })
-    // this.setState({
-    //   areaName: '请选择',
-    //   cityName: value.title
-    // })
-    // fetchRegion({
-    //   parentId: value.key,
-    //   level: 2
-    // }).then((res) => {
-    //   this.setState({
-    //     areaList: res
-    //   })
-    // })
+    if (this.props.onChange) {
+      this.props.onChange(values)
+    }
   }
   public render () {
     return (
       <div className={styles.region}>
+        <label>省份</label>
         <Select
-          mode='multiple'
+          className='mr10'
+          mode='tags'
           labelInValue
           maxTagCount={0}
+          // showArrow={true}
           maxTagPlaceholder={(
-            <span>{this.state.cityName}</span>
+            <span title={this.state.cityName} className={styles['tag-placeholder']}>{this.state.cityName}</span>
           )}
           style={{ width: '120px' }}
           onChange={this.handleCityChange.bind(this)}
@@ -77,25 +111,32 @@ class Main extends React.Component {
             })
           }
         </Select>
-        <DropDown
-          style={{width: '120px'}}
-          className='inline-block'
-          title={this.state.areaName}
-          data={this.state.areaList}
-          onChange={(value) => {
-            this.handleChange({
-              key: 'areaCode',
-              value: value.key
+        <label>市区</label>
+        <Select
+          mode='tags'
+          labelInValue
+          maxTagCount={0}
+          // showArrow={true}
+          value={this.state.areaValues}
+          maxTagPlaceholder={(
+            <span title={this.state.areaName} className={styles['tag-placeholder']}>        {this.state.areaName}
+            </span>
+          )}
+          style={{ width: '120px' }}
+          onChange={this.handleAreaChange.bind(this)}
+        >
+          {
+            this.state.areaList.map((item) => {
+              return (
+                <Option
+                  key={item.code}
+                >
+                {item.name}
+                </Option>
+              )
             })
-            this.setState({
-              areaName: value.title
-            })
-          }}
-          setFields={{
-            title: 'name',
-            key: 'code'
-          }}
-        />
+          }
+        </Select>
       </div>
     )
   }
