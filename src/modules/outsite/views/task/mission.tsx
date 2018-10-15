d3bc2231b13af804246576bb2c37d893a2bd6f71
赵缇 <zhaoti@i-counting.cn>
2018-10-15 12:23:08 +0800

    zhaoti

--- src/modules/outsite/views/task/mission.tsx
+++ src/modules/outsite/views/task/mission.tsx
@@ -4,7 +4,7 @@ const RadioGroup = Radio.Group
 interface States {
   value: number,
   color: string,
-  data: string
+  text: string
 }
 
 function onShowSizeChange (current: any, pageSize: any) {
@@ -14,8 +14,8 @@ function onShowSizeChange (current: any, pageSize: any) {
 class Main extends React.Component<any, any> {
   public state: States = {
     value:1,
-    color: '',
-    data: '注册公司：核名 网上申请 下发执照 刻章'
+    color:'blue',
+    text : '注册公司：核名 网上申请 下发执照 刻章'
   }
   public onChange = (e: any) => {
     console.log('radio checked', e.target.value)
@@ -30,11 +30,11 @@ class Main extends React.Component<any, any> {
     }
     return (
       <div>
-        <RadioGroup onChange={this.onChange} value={this.state.value} className={this.state.color}>
-          <Radio style={radioStyle} value={1}>{this.state.data}</Radio>
-          <Radio style={radioStyle} value={2}>{this.state.data}</Radio>
-          <Radio style={radioStyle} value={3}>{this.state.data}</Radio>
-          <Radio style={radioStyle} value={4}>{this.state.data}</Radio>
+        <RadioGroup onChange={this.onChange} value={this.state.value}>
+          <Radio style={radioStyle} value={1}>{this.state.text}</Radio>
+          <Radio style={radioStyle} value={2}>{this.state.text}</Radio>
+          <Radio style={radioStyle} value={3}>{this.state.text}</Radio>
+          <Radio style={radioStyle} value={4}>{this.state.text}</Radio>
         </RadioGroup>
       </div>
     )
