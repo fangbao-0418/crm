f3283e15979fac44980c396a1378f4946acb6067
gaoyanru <gaoyr@i-counting.cn>
2018-10-22 19:55:50 +0800

    [fix] 分客 related to issue #2

--- src/modules/customer-set/assign/index.tsx
+++ src/modules/customer-set/assign/index.tsx
@@ -59,6 +59,9 @@ class Main extends React.Component<Customer.Props, State> {
                   if (this.state.selectRadio === 2 && !this.state.salesPerson) { // 自定义的时候销售不能为空
                     APP.error('请选择销售')
                     return
+                  } 
+                  if (this.state.selectRadio === 1) {
+                    this.state.salesPerson = []
                   }
                   saveGeneralCapacity(this.state.salesPerson).then(() => {
                     APP.success('操作成功')
