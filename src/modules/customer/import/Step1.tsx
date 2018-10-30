42f842d8947958ebce669f6a36ae9ff5a6784c2b
gaoyanru <gaoyr@i-counting.cn>
2018-10-30 21:04:10 +0800

    [fix] 客资导入 related to issue #2

--- src/modules/customer/import/Step1.tsx
+++ src/modules/customer/import/Step1.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Select, Switch, Button, Tooltip } from 'antd'
+import { Select, Switch, Button, Tooltip, Radio } from 'antd'
 import { fetchRegion } from '@/modules/common/api'
 import { getCompanyByCitycode } from '../api'
 import { getSalesByCompany } from '@/modules/common/api'
 const Option = Select.Option
+const RadioGroup = Radio.Group
 interface State {
-  isChecked: boolean,
+  type: number,
   citys: Common.RegionProps[]
   sales: Array<{id: string, name: string}>
   companys: Array<{id: string, name: string}>
@@ -15,14 +16,17 @@ interface Props {
 }
 interface ValueProps {
   agencyId?: string
-  customerSource?: string,
-  salesPerson?: Array<{id: string, name: string}>,
+  customerSource?: string
+  salesPerson?: Array<{id: string, name: string}>
   city?: {cityCode: string, cityName: string}
+  type?: number
 }
 class Main extends React.Component<Props> {
-  public values: ValueProps = {}
+  public values: ValueProps = {
+    type: 3
+  }
   public state: State = {
-    isChecked: true,
+    type: 3,
     citys: [],
     sales: [],
     companys: []
@@ -47,11 +51,6 @@ class Main extends React.Component<Props> {
       this.setState({
         sales: res
       })
-    })
-  }
-  public onChange (checked: any) {
-    this.setState({
-      isChecked: checked
     })
   }
   public render () {
@@ -109,87 +108,96 @@ class Main extends React.Component<Props> {
           <div>
             <div className='mt12' style={{ textAlign: 'left', marginLeft: 250 }}>
               <span>是否分配：</span>
-              <Switch onChange={this.onChange.bind(this)} defaultChecked/>
+              <RadioGroup
+                onChange={(e) => {
+                  this.setState({
+                    type: e.target.value
+                  })
+                  this.values.type = e.target.value
+                }}
+                value={this.state.type}
+              >
+                <Radio value={3}>转到客资池</Radio>
+                <Radio value={1}>转入公海</Radio>
+                <Radio value={2}>转给销售</Radio>
+              </RadioGroup>
             </div>
-            {
-              this.state.isChecked &&
-              <div>
-                <div className='mt12'>
-                  <span>选择机构：</span>
-                  <Select
-                    style={{width:'200px'}}
-                    onChange={(val: string) => {
-                      this.values.agencyId = val
-                      this.getSales(val)
-                    }}
-                  >
-                    {
-                      this.state.companys.length > 0 &&
-                      this.state.companys.map((item, index) => {
-                        return (
-                          <Option key={item.id}>{item.name}</Option>
-                        )
-                      })
-                    }
-                  </Select>
-                </div>
-                <div className='mt12'>
-                  <span>
-                    <Tooltip placement='top' title='若勾选多个销售，可直接平均分配到各销售库中'>
-                      <i className='fa fa-exclamation-circle ml5' style={{color: '#FBCD5D', marginRight: 5}}></i>
-                    </Tooltip>
-                    分配销售：
-                  </span>
-                  <Select
-                    labelInValue
-                    style={{width:'200px'}}
-                    mode='multiple'
-                    onChange={(val: Array<{key: string, label: string, id: string, name: string}>) => {
-                      const newVal = val.map((item) => {
-                        return {
-                          id: item.key,
-                          name: item.label
-                        }
-                      })
-                      this.values.salesPerson = newVal
-                    }}
-                  >
-                    {
-                      this.state.sales.length > 0 && this.state.sales.map((item, index) => {
-                        return (
-                          <Option key={item.id}>{item.name}</Option>
-                        )
-                      })
-                    }
-                  </Select>
-                </div>
+            <div>
+              <div className='mt12'>
+                <span>选择机构：</span>
+                <Select
+                  style={{width:'200px'}}
+                  onChange={(val: string) => {
+                    this.values.agencyId = val
+                    this.getSales(val)
+                  }}
+                >
+                  {
+                    this.state.companys.length > 0 &&
+                    this.state.companys.map((item, index) => {
+                      return (
+                        <Option key={item.id}>{item.name}</Option>
+                      )
+                    })
+                  }
+                </Select>
               </div>
-            }
+              <div className='mt12'>
+                <span>
+                  <Tooltip placement='top' title='若勾选多个销售，可直接平均分配到各销售库中'>
+                    <i className='fa fa-exclamation-circle ml5' style={{color: '#FBCD5D', marginRight: 5}}></i>
+                  </Tooltip>
+                  分配销售：
+                </span>
+                <Select
+                  labelInValue
+                  style={{width:'200px'}}
+                  mode='multiple'
+                  onChange={(val: Array<{key: string, label: string, id: string, name: string}>) => {
+                    const newVal = val.map((item) => {
+                      return {
+                        id: item.key,
+                        name: item.label
+                      }
+                    })
+                    this.values.salesPerson = newVal
+                  }}
+                >
+                  {
+                    this.state.sales.length > 0 && this.state.sales.map((item, index) => {
+                      return (
+                        <Option key={item.id}>{item.name}</Option>
+                      )
+                    })
+                  }
+                </Select>
+              </div>
+            </div>
           </div>
         <div className='text-right mt10'>
           <Button
             type='primary'
             onClick={() => {
               // console.log(this.values, 'values 导入')
-              if (this.values.city && this.values.customerSource) {
-                if (this.state.isChecked) {
-                  if (this.values.agencyId && this.values.salesPerson) {
-                    if (this.props.onOk) {
-                      this.props.onOk(this.values)
-                    }
-                  } else {
-                    APP.error('请选择机构和销售')
-                  }
-                } else {
-                  this.values.salesPerson = []
-                  this.values.agencyId = ''
-                  if (this.props.onOk) {
-                    this.props.onOk(this.values)
-                  }
-                }
-              } else {
-                APP.error('请选择客户来源和城市')
-              }
+              // if (this.values.city && this.values.customerSource) {
+              //   if (this.state.isChecked) {
+              //     if (this.values.agencyId && this.values.salesPerson) {
+              //       if (this.props.onOk) {
+              //         this.props.onOk(this.values)
+              //       }
+              //     } else {
+              //       APP.error('请选择机构和销售')
+              //     }
+              //   } else {
+              //     this.values.salesPerson = []
+              //     this.values.agencyId = ''
+              //     if (this.props.onOk) {
+              //       this.props.onOk(this.values)
+              //     }
+              //   }
+              // } else {
+              //   APP.error('请选择客户来源和城市')
+              // }
             }}
           >
             下一步
