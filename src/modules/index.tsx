b211b55e8102b3fb8d928fc88546b1db3b2017ad
fangbao <fangbao@i-counting.cn>
2018-10-31 14:37:42 +0800

    [fix] 整合路由 related to issue #1

--- src/modules/index.tsx
+++ src/modules/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { withRouter, RouteComponentProps } from 'react-router'
 class Main extends React.Component<RouteComponentProps<{}>> {
   public componentWillMount () {
-    if (APP.homepage !== '/' && window.location.pathname === '/') {
+    if (APP.homepage && window.location.pathname === '/') {
       window.location.href = APP.homepage
     }
   }
