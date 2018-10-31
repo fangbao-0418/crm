5e301b260ba042cf136165f3e78a9cb58448e653
fangbao <fangbao@i-counting.cn>
2018-10-31 14:48:22 +0800

    [fix] 整合路由 related to issue #1

--- src/modules/common/left/Iframe.tsx
+++ src/modules/common/left/Iframe.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 const styles = require('./style')
 class Main extends React.Component {
   public render () {
+    const url = APP.env === 'production' ? '/menu' : '/#menu'
     return (
       <iframe
         width='200px'
         height='100%'
         className={styles.iframe}
-        src='/#menu'
+        src={url}
         frameBorder='0'
       />
     )
