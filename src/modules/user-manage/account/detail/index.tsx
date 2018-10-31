d17abd22e32fac768edbb7a1cdd982065f3cc3e9
gaoyanru <gaoyr@i-counting.cn>
2018-10-31 19:07:43 +0800

    [fix] 修改手机格式 related to issue #2

--- src/modules/user-manage/account/detail/index.tsx
+++ src/modules/user-manage/account/detail/index.tsx
@@ -197,7 +197,7 @@ class Main extends React.Component<Props, State> {
         validateTrigger: 'onBlur',
         rules:[
           {required: true, message: '请输入手机号！'},
-          {pattern: /^1[3|4|5|7|8|9][0-9]\d{8}$/, message: '手机号格式不对！'}
+          {pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/, message: '手机号格式不对！'}
         ]
       },
       email: {
