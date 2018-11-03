860ea0f991bf71a82fc7031bb7e2eba711de40bf
fangbao <fangbao@i-counting.cn>
2018-11-03 14:27:32 +0800

    [fix] 数据明细 related to issue #1

--- src/modules/data-detail/index.tsx
+++ src/modules/data-detail/index.tsx
@@ -5,16 +5,28 @@ import Personal from '@/modules/data-detail/views/personal'
 import Task from '@/modules/data-detail/views/task'
 const TabPane = Tabs.TabPane
 const styles = require('./styles/index.styl')
-class Main extends React.Component {
+interface State {
+  tab: '1' | '2'
+}
+class Main extends React.Component<{}, State> {
+  public state: State = {
+    tab: '1'
+  }
+  public callBack (value: any) {
+    this.setState({
+      tab: value
+    })
+  }
   public render () {
+    const { tab } = this.state
     return (
       <ContentBox title='数据明细'>
-        <Tabs defaultActiveKey='1'>
+        <Tabs defaultActiveKey='1' onChange={this.callBack.bind(this)}>
           <TabPane tab='外勤人员' key='1'>
-            <Personal/>
+            {tab === '1' && <Personal/>}
           </TabPane>
           <TabPane tab='外勤任务' key='2'>
-            <Task />
+            {tab === '2' && <Task />}
           </TabPane>
         </Tabs>
       </ContentBox>
