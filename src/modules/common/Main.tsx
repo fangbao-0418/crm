6b1a09b9b3a101a28fdbff8ea706ba59910ddc31
fangbao <fangbao@i-counting.cn>
2018-11-07 19:07:24 +0800

    [fix] 外勤 related to issue #1

--- src/modules/common/Main.tsx
+++ src/modules/common/Main.tsx
@@ -13,8 +13,16 @@ class Main extends React.Component<Common.Props> {
           APP.user = user
         }}
       >
-        <Route hidden={!APP.hasPermission('track_work_order')} path='/workorder/list' component={modules.WorkorderList}/>
-        <Route path='/workorder/show/:id' component={modules.WorkorderShow}/>
+        <Route hidden={!APP.hasPermission('track_outside_task_list')} path='/outsite/task/list' component={modules.TaskList}/>
+        <Route path='/outsite/task/show/:id' component={modules.TaskShow}/>
+        <Route hidden={!APP.hasPermission('track_outside_task_add')} path='/outsite/task/form' component={modules.TaskForm}/>
+        <Route hidden={!APP.hasPermission('track_outside_tasktemplate_main')} path='/outsite/tasktpl/list' component={modules.TasktplList}/>
+        <Route path='/outsite/tasktpl/form/:id?' component={modules.TasktplForm}/>
+        <Route hidden={!APP.hasPermission('track_outside_tasktemplate_sub')} path='/outsite/tasktpl/sublist' component={modules.TasktplSublist}/>
+        <Route path='/outsite/tasktpl/subform/:id?' component={modules.TasktplSubform}/>
+        <Route hidden={!APP.hasPermission('track_outside_reward_config')} path='/outsite/perform/list' component={modules.PerformList}/>
+        <Route hidden={!APP.hasPermission('track_outside_data_overview')} path='/outsite/data-overview' component={modules.TaskDataOverview}/>
+        <Route hidden={!APP.hasPermission('track_outside_data_detail')} path='/outsite/data-detail' component={modules.TaskDataDetail}/>
       </Iframe>
     )
   }
