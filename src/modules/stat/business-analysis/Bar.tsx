31837ad9bac8637f889344ad8375d8817b40ddfc
林颖 <linying@i-counting.cn>
2018-12-20 18:06:08 +0800

    [feat] 柱形图宽度 related to issue #1

--- src/modules/stat/business-analysis/Bar.tsx
+++ src/modules/stat/business-analysis/Bar.tsx
@@ -83,7 +83,6 @@ class Main extends React.Component<any> {
         name: '客户数量',
         type: 'bar',
         barWidth: '40',
-        // barMaxWidth: '40%',
         data: char.map((item: any) => {
           return item.statusNums
         })
