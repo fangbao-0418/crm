c0008223017706a8de5649c3face78a015544826
林颖 <linying@i-counting.cn>
2018-12-26 18:59:54 +0800

    [feat] 按城市查询 权限 related to issue #1

--- src/modules/stat/customer-sign/CityRank.tsx
+++ src/modules/stat/customer-sign/CityRank.tsx
@@ -6,17 +6,22 @@ const styles = require('./style')
 class Main extends React.Component<any> {
   public columns: ColumnProps<CrmStat.TotalByCityDetails>[] = [
     {
-      title: '城市排名',
+      title: '排名',
       width: 300,
-      align: 'left',
-      dataIndex: 'totalByCity.name',
       render: (text, record) => {
         return (
           <span>
             {record.key > 3 ? <span className={styles.ran}>{record.key}</span> : <span className={styles.rank}>{record.key}</span>}
-            <span>{record.name}</span>
           </span>
         )
+      }
+    },
+    {
+      title: '城市排名',
+      width: 300,
+      dataIndex: 'totalByCity.name',
+      render: (text, record) => {
+        return record.name
       }
     },
     {
