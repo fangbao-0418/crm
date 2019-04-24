3d12f2dce610e09f25f48589180b169dc6fa1507
林颖 <linying@i-counting.cn>
2019-04-24 16:13:25 +0800

    [feat] 销售转化仪表盘文本 related to issue #1

--- src/modules/stat/business-analysis/index.tsx
+++ src/modules/stat/business-analysis/index.tsx
@@ -6,7 +6,7 @@ class Main extends React.Component {
   public render () {
     return (
       <ContentBox
-        title='商机分析仪表盘'
+        title='销售转化仪表盘'
       >
         <BusinessDetail />
       </ContentBox>
