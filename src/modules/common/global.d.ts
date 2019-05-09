447d9eee719abd96be2ad4e73de947dcf58999a9
gaoyanru <gaoyr@i-counting.cn>
2019-05-09 17:33:28 +0800

    [fix] 看版 related to issue #1

--- src/modules/common/global.d.ts
+++ src/modules/common/global.d.ts
@@ -16,6 +16,8 @@ declare namespace Common {
     /** tq类型 0: 无, 1: TQ云呼叫, 2: 易米云通, 3: 工作手机 */
     tqType?: 0 | 1 | 2 | 3
     userType?: 'System' | 'DirectCompany' | 'Agent'
+    roleName?: string
+    organizationName?: string
   }
   export type ActionPayload = Props
   export interface RegionProps {
--- src/modules/sale-board/index.tsx
+++ src/modules/sale-board/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import classNames from 'classnames'
 import { Table } from 'antd'
 import { fetchInfo } from './api'
+import _ from 'lodash'
 const styles = require('./style')
 interface State {
   type?: 'CALL' | 'ACHIEVEMENT'
@@ -29,56 +30,62 @@ interface State {
     /** 今天新签排行 */
     signedToday?: Array<{sortNumber?: string, salesperson?: string, value?: string}>
   }
-  data?: Array<{rank?: string, naver?: string, sale?: string, num?: string}>
+  callTodayList?: Array<{sortNumber?: string, salesperson?: string, value?: string}>
+  signedTodayList?: Array<{sortNumber?: string, salesperson?: string, value?: string}>
+  conditionTodayList?: Array<{salespersonName?: string, createTime?: string, customerName?: string}>
 }
 class Main extends React.Component<{}, State> {
   public columns = [{
     title: '排名',
-    dataIndex: 'rank'
+    dataIndex: 'sortNumber'
   }, {
     title: '头像',
-    dataIndex: 'naver',
+    dataIndex: 'salesperson',
     render: (val: string) => {
       return (
-        <span className={styles.naver}>{val}</span>
+        <span className={styles.naver}>{val.slice(0, 1)}</span>
       )
     }
   }, {
     title: '销售',
-    dataIndex: 'sale'
+    dataIndex: 'salesperson'
   }, {
     title: '通话量',
-    dataIndex: 'num'
+    dataIndex: 'value'
   }]
   public state: State = {
     type: 'CALL',
     dataSource: {},
-    data: [{
-      rank: '4',
-      naver: '李',
-      sale: '张大大',
-      num: '50000'
-    }]
+    callTodayList: [],
+    signedTodayList: [],
+    conditionTodayList: []
   }
   public componentWillMount () {
     fetchInfo().then((res) => {
       this.setState({
-        dataSource: res.data
+        dataSource: res.data,
+        conditionTodayList: res.data.conditionToday,
+        callTodayList: res.data.callToday,
+        signedTodayList: res.data.signedToday
       })
     })
   }
   public render () {
+    const { dataSource, conditionTodayList, callTodayList, signedTodayList } = this.state
+    const curData: Array<{sortNumber?: string, salesperson?: string, value?: string}> = this.state.type === 'CALL' ? callTodayList : signedTodayList
+    const data = _.cloneDeep(curData)
+    data.splice(0, 3)
     return (
       <div className={classNames(styles.board)}>
         <div className={classNames(styles.top, 'clear')}>
           <div className={classNames('fl', 'bg', styles.card)}>
             <img src={require('@/assets/images/touxiang.png')}/>
             <div>
-              <p className={styles.title}>Hellow，王大大，祝你每一天都有新收获哦！加油～</p>
+              <p className={styles.title}>{dataSource.kanbanWelcoming}</p>
               <p>
-                <span>销售顾问</span>
+                <span>{APP.user.roleName}</span>
                 <span> | </span>
-                <span>北京直营-销售部-雷霆队</span>
+                <span>{APP.user.organizationName}</span>
               </p>
             </div>
           </div>
@@ -87,21 +94,21 @@ class Main extends React.Component<{}, State> {
               <img src={require('@/assets/images/我的商机icon.png')}/>
               <div>
                 <p>商机客户</p>
-                <p className={styles.num}>19，988</p>
+                <p className={styles.num}>{dataSource.businessCustomerNums}</p>
               </div>
             </li>
             <li className={classNames('bg')}>
               <img src={require('@/assets/images/本月完成新签.png')}/>
               <div>
                 <p>意向客户</p>
-                <p className={styles.num}>20</p>
+                <p className={styles.num}>{dataSource.infoDegree}</p>
               </div>
             </li>
             <li className={classNames('bg')}>
               <img src={require('@/assets/images/本月跟进.png')}/>
               <div>
                 <p>本月新签客户</p>
-                <p className={styles.num}>5，000</p>
+                <p className={styles.num}>{dataSource.signedNums}</p>
               </div>
             </li>
           </ul>
@@ -109,19 +116,19 @@ class Main extends React.Component<{}, State> {
         <ul className={classNames('mt10', styles.middle, 'clear')}>
           <li className={styles.first}>
             <p>待跟进</p>
-            <p className={styles.num}>20</p>
+            <p className={styles.num}>{dataSource.awaitTrackNums}</p>
           </li>
           <li className={styles.second}>
             <p>今日待回访</p>
-            <p className={styles.num}>20</p>
+            <p className={styles.num}>{dataSource.returnVisitNums}</p>
           </li>
           <li className={styles.third}>
             <p>信息待完善</p>
-            <p className={styles.num}>20</p>
+            <p className={styles.num}>{dataSource.intentionCustomerNums}</p>
           </li>
           <li className={styles.forth}>
             <p>下单进行中</p>
-            <p className={styles.num}>20</p>
+            <p className={styles.num}>{dataSource.signingNums}</p>
           </li>
         </ul>
         <div className={classNames('mt10', styles.bottom, 'clear')}>
@@ -151,38 +158,41 @@ class Main extends React.Component<{}, State> {
                   今日业绩榜
                 </span>
               </div>
-              <ul className={classNames(styles['tab-con'], 'clear')}>
-                <li className={classNames(styles['no-first'], styles.third)}>
-                  <div className={styles.box}>
-                    <img src={require('@/assets/images/第三名.png')}/>
-                    <p className={styles.circle}>高</p>
-                  </div>
-                  <p className={styles.name}>高圆圆</p>
-                  <p className={styles.num}>通话量：300</p>
-                </li>
-                <li className={styles.first}>
-                  <div>
-                    <img src={require('@/assets/images/第一名.png')}/>
-                    <p className={styles.circle}>陈</p>
-                  </div>
-                  <p className={styles.name}>陈大大</p>
-                  <p className={styles.num}>通话量：50000</p>
-                </li>
-                <li className={classNames(styles['no-first'], styles.second)}>
-                  <div className={styles.box}>
-                    <img src={require('@/assets/images/第二名.png')}/>
-                    <p className={styles.circle}>
-                      凯
-                    </p>
-                  </div>
-                  <p className={styles.name}>凯丽</p>
-                  <p className={styles.num}>通话量：400</p>
-                </li>
-              </ul>
+              {
+                curData.length > 0 &&
+                <ul className={classNames(styles['tab-con'], 'clear')}>
+                  <li className={classNames(styles['no-first'], styles.third)}>
+                    <div className={styles.box}>
+                      <img src={require('@/assets/images/第三名.png')}/>
+                      <p className={styles.circle}>{curData[2] && curData[2].salesperson ? curData[2].salesperson.slice(0, 1) : '未'}</p>
+                    </div>
+                    <p className={styles.name}>{curData[2] && curData[2].salesperson}</p>
+                    <p className={styles.num}>通话量：{curData[2] && curData[2].value}</p>
+                  </li>
+                  <li className={styles.first}>
+                    <div>
+                      <img src={require('@/assets/images/第一名.png')}/>
+                      <p className={styles.circle}>{curData[0] && curData[0].salesperson ? curData[0].salesperson.slice(0, 1) : '未'}</p>
+                    </div>
+                    <p className={styles.name}>{curData[0] && curData[0].salesperson}</p>
+                    <p className={styles.num}>通话量：{curData[0] && curData[0].value}</p>
+                  </li>
+                  <li className={classNames(styles['no-first'], styles.second)}>
+                    <div className={styles.box}>
+                      <img src={require('@/assets/images/第二名.png')}/>
+                      <p className={styles.circle}>
+                        {curData[1] && curData[1].salesperson ? curData[1].salesperson.slice(0, 1) : '未'}
+                      </p>
+                    </div>
+                    <p className={styles.name}>{curData[1] && curData[1].salesperson}</p>
+                    <p className={styles.num}>通话量：{curData[1] && curData[1].value}</p>
+                  </li>
+                </ul>
+              }
               <div className={classNames('mt20', styles['tab-table'])}>
                 <Table
                   columns={this.columns}
-                  dataSource={this.state.data}
+                  dataSource={data}
                   pagination={false}
                 />
               </div>
@@ -191,27 +201,24 @@ class Main extends React.Component<{}, State> {
           <div className={styles.dynamic}>
             <div className='clear'>
               <span className={classNames(styles.title, 'fl')}>客资动态</span>
-              <span className={classNames(styles.more, 'fr')} onClick={() => APP.history.push('/crm/business')}>查看更多></span>
+              <span className={classNames(styles.more, 'fr')} onClick={() => APP.history.push('/business')}>查看更多></span>
             </div>
             <ul>
-              <li>
-                <p>
-                  <span>张磊 </span>
-                  <span> 将客户</span>
-                  <span className={styles.company}>北京爱康定科技有限公司</span>
-                  <span>分给你</span>
-                </p>
-                <p>12:09</p>
-              </li>
-              <li>
-                <p>
-                  <span>张磊 </span>
-                  <span> 将客户</span>
-                  <span className={styles.company}>北京爱康定科技有限公司</span>
-                  <span>分给你</span>
-                </p>
-                <p>12:09</p>
-              </li>
+              {
+                conditionTodayList.length > 0 && conditionTodayList.map((item, index) => {
+                  return (
+                    <li key={index}>
+                      <p>
+                        <span>{item.salespersonName} </span>
+                        <span> 将客户</span>
+                        <span className={styles.company} onClick={() => {APP.history.push('/business')}}>{item.customerName}</span>
+                        <span>分给你</span>
+                      </p>
+                      <p>{item.createTime}</p>
+                    </li>
+                  )
+                })
+              }
             </ul>
           </div>
         </div>
